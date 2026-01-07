e6b758d44eeb7bce113d0484e11d4c96bd146119
Shyam Mantri <iamshyam1911@gmail.com>
2026-01-07 22:01:46 +0530

    Create script.js

---
+++ JavaScript/Subscribe to Us/script.js
@@ -0,0 +1,26 @@
+let subscribeFormEl = document.getElementById("subscribeForm");
+let nameEl = document.getElementById("name");
+let emailEl = document.getElementById("email");
+let nameErrMsgEl = document.getElementById("nameErrMsg");
+let emailErrMsgEl = document.getElementById("emailErrMsg");
+let subscribeBtnEl = document.getElementById("subscribeBtn");
+
+subscribeBtnEl.addEventListener("click", function (event) {
+  event.preventDefault();
+});
+
+nameEl.addEventListener("blur", function (event) {
+  if (event.target.value === "") {
+    nameErrMsgEl.textContent = "Required*";
+  } else {
+    nameErrMsgEl.textContent = "";
+  }
+});
+
+emailEl.addEventListener("blur", function (event) {
+  if (event.target.value === "") {
+    emailErrMsgEl.textContent = "Required*";
+  } else {
+    emailErrMsgEl.textContent = "";
+  }
+});
